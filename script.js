bb024b4c2c0dadf3035c15fce427ce209a1c473c
Nazia R <nazia.huma@gmail.com>
2021-04-01 18:47:53 +0100

    added data attribute to html

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const m = moment();
+const timeBlocks = $(".container .row");
+console.log(timeBlocks);
+const timeBlockArray = [9,10,11,12,13,14,15,16,17]; 
+const past = $(".past");
+const present = $(".present");
+const future = $(".future");
+
+//console.log(timeBlockArray);
 
 const getCurrentDateTime = () => {
   //get current date and time
@@ -16,6 +24,16 @@ const startTime = () => {
   const timerInterval = setInterval(callBack, 1000);
 };
 
-
-
 $("document").ready(startTime());
+
+const UpdateTimeBlock = (event) => {
+
+    if (12 === getCurrentDateTime()) {
+      
+    }
+ //console.log(event)
+  //get current hour
+};
+
+
+timeBlockArray.forEach(UpdateTimeBlock);
