e624702ccafeb0c905f15d2781dfc1c6434a55fa
Nazia R <nazia.huma@gmail.com>
2021-04-02 08:33:21 +0100

    added event listener to save button

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-const m = moment();
 const timeBlocks = $(".container .row");
-console.log(timeBlocks);
 const timeBlockArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 const past = $(".past");
 const present = $(".present");
 const future = $(".future");
+const buttonElement = $(".saveBtn");
 const currentHour = moment().hour();
 
 //console.log(timeBlockArray);
 
 const getCurrentDateTime = () => {
   //get current date and time
-  const dateTime = m.format("dddd MMM Do YYYY, HH:mm:ss");
+  const dateTime = moment().format("dddd MMM Do YYYY, HH:mm:ss");
 
   return dateTime;
 };
@@ -22,26 +21,35 @@ const startTime = () => {
     $("#currentDay").text(renderDateTime);
   };
 
-  const timerInterval = setInterval(callBack, 1000);
+  setInterval(callBack, 1000);
 };
 
-$("document").ready(startTime());
+$("document").ready(startTime);
 
 const UpdateTimeBlock = (event) => {
   const hour = parseInt($(this).attr("data-mytime"));
   if (hour < currentHour) {
-        container.remove(future);
-        container.append(past);
-        console.log(UpdateTimeBlock);
-  } else if (hour === currentHour) {
-      container.remove(past);
-      container.append(present);
-  } else if (hour > currentHour) {
-        container.remove(past);
-        container.append(future);
-  } else {
-      
+    container.remove(future);
+    container.append(past);
+    console.log(UpdateTimeBlock);
+  }
+  if (hour === currentHour) {
+    container.remove(past);
+    container.append(present);
+  }
+  if (hour > currentHour) {
+    container.remove(past);
+    container.append(future);
   }
 };
 
 timeBlockArray.forEach(UpdateTimeBlock);
+
+const saveText = () => {};
+
+const addEventListenerOnBtn = (forEachBtn) => {
+  forEachBtn.addEventListener("click, saveText");
+  console.log(addEventListenerOnBtn);
+};
+
+buttonElement.forEach(addEventListenerOnBtn);
